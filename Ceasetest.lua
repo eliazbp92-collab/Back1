a64626425e2f7a76519ad38896017afc4e118ab0
Rebound <eliazbp92@gmail.com>
2026-08-30 11:03:41 -0700

    Update Ceasetest.lua

--- Ceasetest.lua
+++ Ceasetest.lua
@@ -106,7 +106,7 @@ entity:SetCallback("OnSpawned", function()
 		local tweenInfo = TweenInfo.new(10, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
 
 		tw = TweenService:Create(blueSound, tweenInfo, {
-			PlaybackSpeed = 2
+			PlaybackSpeed = 5
 		})
 		tw:Play()
 		
