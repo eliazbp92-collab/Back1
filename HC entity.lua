35179aab749b1b2bcd3a97f9d1dd9e619795f9bd
Rebound <eliazbp92@gmail.com>
2026-08-30 19:24:55 -0700

    Update HC entity.lua

--- HC entity.lua
+++ HC entity.lua
@@ -2,7 +2,7 @@ local workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local Paritvle = "https://github.com/eliazbp92-collab/Back1/blob/main/Place_131351567799504_Model_RushMoving_1788142788.rbxm"
+local Paritvle = "https://github.com/eliazbp92-collab/Back1/raw/main/Place_131351567799504_Model_RushMoving_1788142788.rbxm"
 local cachedAttachmentTemplate = nil -- Lưu trữ model tạm thời vào bộ nhớ
 
 -- Hàm tải model từ GitHub và cache lại
