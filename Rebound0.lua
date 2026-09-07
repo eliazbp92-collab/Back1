699e4f93343c17f918b8490dd1d4e0a529be0e8c
Rebound <eliazbp92@gmail.com>
2026-09-07 12:40:21 -0700

    Update Rebound0.lua

--- Rebound0.lua
+++ Rebound0.lua
@@ -126,6 +126,7 @@ local entity = Creator.createEntity({
 -----[[ Advanced ]]-----
 entity.Debug.OnEntitySpawned = function(entityTable)
     print("Entity has spawned:", entityTable.Model)
+	light(2, Color3.fromRGB(127, 249, 255),Color3.fromRGB(65, 138, 255)
 end
 
 entity.Debug.OnEntityDespawned = function(entityTable)
