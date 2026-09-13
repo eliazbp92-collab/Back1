ced35b3210592c34a197e113c7ebab064e24dd27
Rebound <eliazbp92@gmail.com>
2026-09-13 11:46:47 -0700

    Update Older Cease.lua

--- Older Cease.lua
+++ Older Cease.lua
@@ -19,7 +19,7 @@ local Creator = loadstring(game:HttpGet("https://pastebin.com/raw/0fSnvfGt"))()
 -- Create entity
 local entity = Creator.createEntity({
     CustomName = "Cease", -- Custom name of your entity
-    Model = "13197667688", -- Can be GitHub file or rbxassetid
+    Model = "rbxassetid://13197667688", -- Can be GitHub file or rbxassetid
     Speed = 65, -- Percentage, 100 = default Rush speed
     DelayTime = 3, -- Time before starting cycles (seconds)
     HeightOffset = 0,
